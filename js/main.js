13940c9f18b5d0afa0179b51a4455b00d6706945
Chet Gray <chetgray@gmail.com>
2018-02-02 01:09:52 -0500

    Set x ticks to 60 (second) intervals

--- js/main.js
+++ js/main.js
@@ -11,6 +11,14 @@ var chart = c3.generate({
     x: 'ROAST TIME',
     columns: data
   },
+  axis: {
+    x: {
+      tick: {
+        values: [0,60,120,180,240,300,360,420,480,540,600,
+          660,720,780,840,900,960,1020,1080,1140,1200]
+      }
+    }
+  },
   grid: {
     x: {
       show: true
