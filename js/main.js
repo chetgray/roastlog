9417fd955022f4be8bdfc773d80c4e0f395118d4
Chet Gray <chetgray@gmail.com>
2018-02-08 18:29:17 -0500

    Use a lambda for the x ticks

--- js/main.js
+++ js/main.js
@@ -14,8 +14,7 @@ var chart = c3.generate({
   axis: {
     x: {
       tick: {
-        values: [0,60,120,180,240,300,360,420,480,540,600,
-          660,720,780,840,900,960,1020,1080,1140,1200]
+        values: (function(){var a = []; for (i = 0; i <= 1200; i += 60) {a.push(i)}; return a;})()
       }
     }
   },
